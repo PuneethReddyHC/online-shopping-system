9370afb0231cec08911de43bea53927dd806ca21
Puneeth Reddy H C <puneethreddy951@gmail.com>
2018-11-18 22:19:57 +0530

    Add files via upload

---
+++ admin/sidenav.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Hello, world!</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="assets/css/Material+Icons.css" />
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+  <!-- Material Kit CSS -->
+  <link href="assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+  <link href="assets/css/black-dashboard.css" rel="stylesheet" />
+</head>
+
+<body class="dark-edition">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="purple" data-background-color="black" data-image="./assets/img/sidebar-2.jpg">
+      <!--
+      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+      Tip 2: you can also add an image using data-image tag
+  -->
+      <div class="logo">
+        <a href="" class="simple-text logo-normal">
+          PuneethReddy
+        </a>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li class="nav-item  ">
+            <a class="nav-link" href="index.php">
+              <i class="material-icons">dashboard</i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li class="nav-item active ">
+            <a class="nav-link" href="adduser.php">
+              <i class="material-icons">person</i>
+              <p>Add User</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="productlist.php">
+              <i class="material-icons">list</i>
+              <p>Product List</p>
+            </a>
+            
+          </li>
+          
+          <li class="nav-item ">
+            <a class="nav-link" href="orders.php">
+              <i class="material-icons">library_books</i>
+              <p>Orders</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="addproduct.php">
+              <i class="material-icons">add</i>
+              <p>Add Products</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="manageuser.php">
+              <i class="material-icons">edit_user</i>
+              <p>Manage User</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="../logout.php">
+              <i class="material-icons">logout</i>
+              <p>Logout</p>
+            </a>
+          </li>
+          <!-- <li class="nav-item active-pro ">
+                <a class="nav-link" href="./upgrade.html">
+                    <i class="material-icons">unarchive</i>
+                    <p>Upgrade to PRO</p>
+                </a>
+            </li> -->
+        </ul>
+      </div>
+    </div>
+    
---
+++ admin/topheader.php
@@ -0,0 +1,28 @@
+<div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <a class="navbar-brand" href="javascript:void(0)">Dashboard</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">
+                  <i class="material-icons">notifications</i>
+                  <p class="d-lg-none d-md-block">
+                    Notifications
+                  </p>
+                </a>
+              </li>
+              <!-- your navbar here -->
+            </ul>
+          </div>
+        </div>
+      </nav>
